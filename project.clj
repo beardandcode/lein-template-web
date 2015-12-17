85308be8a1279fdd14fb1704011ea3aa27db7624
Tom Booth <tombooth@gmail.com>
2015-12-17 19:01:36 +0000

    Working on 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.beardandcode.web/lein-template "0.1.0"
+(defproject com.beardandcode.web/lein-template "0.1.1"
   :description "A template to generate web applications"
   :url "https://github.com/beardandcode/lein-template-web"
   :license {:name "MIT"
